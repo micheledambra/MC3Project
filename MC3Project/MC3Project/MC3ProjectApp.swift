41b5a07e941a85c84025996d905702ae903f7e43
micheledambra <micheledambra@users.noreply.github.com>
2023-02-13 12:05:15 +0100

    pro commit

---
+++ MC3Project/MC3Project/MC3ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC3ProjectApp.swift
+//  MC3Project
+//
+//  Created by Michele D'Ambra on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MC3ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
