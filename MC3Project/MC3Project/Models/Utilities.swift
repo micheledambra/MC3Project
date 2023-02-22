03c25d2d8e75496b2c713ea8d3ae68f5349ab61e
Simon Bestler <68077359+bestler@users.noreply.github.com>
2023-02-22 20:28:43 +0100

    created Utilities (small Structs for better structuring the code)

---
+++ MC3Project/MC3Project/Models/Utilities.swift
@@ -0,0 +1,23 @@
+//
+//  Utilities.swift
+//  MC3Project
+//
+//  Created by Simon Bestler on 22.02.23.
+//
+
+import Foundation
+
+struct Position {
+    let x = 0
+    let y = 0
+}
+
+struct ColorIntesities {
+    let red = 0.0
+    let green = 0.0
+    let blue = 0.0
+}
+
+enum RGB {
+    case red, green, blue
+}
