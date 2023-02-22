d13d5bba4d8925546bd71858384684d6346b7e15
micheledambra <micheledambra@users.noreply.github.com>
2023-02-22 10:41:31 +0100

    Posix to RGB (to rebuild according to Xcode project)

---
+++ MC3Project/MC3Project/PosixToRGB.swift
@@ -0,0 +1,31 @@
+//
+//  PosixToRGB.swift
+//  MC3Project
+//
+//  Created by Michele D'Ambra on 22/02/23.
+//
+
+import Foundation
+import UIKit
+
+/*
+let image = UIImage(named: "rgb.png")!
+
+guard let cgImage = image.cgImage,
+      let data = cgImage.dataProvider?.data,
+      let bytes = CFDataGetBytePtr(data) else {
+    fatalError("Couldn't access image data")
+}
+assert(cgImage.colorSpace?.model == .rgb)
+
+let bytesPerPixel = cgImage.bitsPerPixel / cgImage.bitsPerComponent
+
+func getRGB(x:Int, y:Int){
+    let offset = (y * cgImage.bytesPerRow) + (x * bytesPerPixel)
+    let components = (r: bytes[offset], g: bytes[offset + 1], b: bytes[offset + 2])
+    print("[x:\(x), y:\(y)] \(components)")
+    return
+}
+
+getRGB(x: 2, y: 0)
+*/
