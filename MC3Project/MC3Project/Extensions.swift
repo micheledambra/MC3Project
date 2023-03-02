ea29a5d0ff9f4a4581b3def886ea3cde42811842
Simon Bestler <68077359+bestler@users.noreply.github.com>
2023-03-02 09:19:43 +0100

    Rotate underlying imageData according to imageOrientation, when image is getting scaled

--- MC3Project/MC3Project/Extensions.swift
+++ MC3Project/MC3Project/Extensions.swift
@@ -12,7 +12,7 @@ import UIKit
 extension UIImage {
 
     func resizeByScalingFactor(_ scaleFactor : Double) -> UIImage? {
-        let cgImage = self.cgImage!
+        let cgImage = self.cgImage!.rotating(to: CGImagePropertyOrientation(self.imageOrientation))!
 
         let destWidth = Int(self.size.width * scaleFactor)
         let destHeight = Int(self.size.height * scaleFactor)
@@ -41,6 +41,122 @@ extension UIImage {
             return newImage
         } else {
             return nil
+        }
+    }
+}
+
+extension CGImage {
+    func rotating(to orientation: CGImagePropertyOrientation) -> CGImage? {
+        var orientedImage: CGImage?
+        
+        let originalWidth = self.width
+        let originalHeight = self.height
+        let bitsPerComponent = self.bitsPerComponent
+        let bitmapInfo = self.bitmapInfo
+        
+        guard let colorSpace = self.colorSpace else {
+            return nil
+        }
+        
+        var degreesToRotate: Double
+        var swapWidthHeight: Bool
+        var mirrored: Bool
+        
+        switch orientation {
+        case .up:
+            degreesToRotate = 0.0
+            swapWidthHeight = false
+            mirrored = false
+            break
+        case .upMirrored:
+            degreesToRotate = 0.0
+            swapWidthHeight = false
+            mirrored = true
+            break
+        case .right:
+            degreesToRotate = -90.0
+            swapWidthHeight = true
+            mirrored = false
+            break
+        case .rightMirrored:
+            degreesToRotate = -90.0
+            swapWidthHeight = true
+            mirrored = true
+            break
+        case .down:
+            degreesToRotate = 180.0
+            swapWidthHeight = false
+            mirrored = false
+            break
+        case .downMirrored:
+            degreesToRotate = 180.0
+            swapWidthHeight = false
+            mirrored = true
+            break
+        case .left:
+            degreesToRotate = 90.0
+            swapWidthHeight = true
+            mirrored = false
+            break
+        case .leftMirrored:
+            degreesToRotate = 90.0
+            swapWidthHeight = true
+            mirrored = true
+            break
+        }
+        
+        let radians = degreesToRotate * Double.pi / 180.0
+        
+        var width: Int
+        var height: Int
+        
+        if swapWidthHeight {
+            width = originalHeight
+            height = originalWidth
+        } else {
+            width = originalWidth
+            height = originalHeight
+        }
+        
+        let bytesPerRow = (width * bitsPerPixel) / 8
+        
+        let contextRef = CGContext(data: nil, width: width, height: height, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)
+        
+        contextRef?.translateBy(x: CGFloat(width) / 2.0, y: CGFloat(height) / 2.0)
+        
+        if mirrored {
+            contextRef?.scaleBy(x: -1.0, y: 1.0)
+        }
+        
+        contextRef?.rotate(by: CGFloat(radians))
+        
+        if swapWidthHeight {
+            contextRef?.translateBy(x: -CGFloat(height) / 2.0, y: -CGFloat(width) / 2.0)
+        } else {
+            contextRef?.translateBy(x: -CGFloat(width) / 2.0, y: -CGFloat(height) / 2.0)
+        }
+        
+        contextRef?.draw(self, in: CGRect(x: 0.0, y: 0.0, width: CGFloat(originalWidth), height: CGFloat(originalHeight)))
+        
+        orientedImage = contextRef?.makeImage()
+        
+        return orientedImage
+    }
+}
+
+extension CGImagePropertyOrientation {
+    init(_ uiOrientation: UIImage.Orientation) {
+        switch uiOrientation {
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
+        @unknown default:
+            fatalError("Image orientation not known")
         }
     }
 }
