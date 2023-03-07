e17f69df776112ff0706b5353e286d76c5f1908b
micheledambra <micheledambra@users.noreply.github.com>
2023-03-07 12:28:51 +0100

    fixed corner radius in onboarding

--- MC3Project/MC3Project/ViewModels/OnboardingButtonStyle.swift
+++ MC3Project/MC3Project/ViewModels/OnboardingButtonStyle.swift
@@ -13,7 +13,7 @@ struct OnboardingButtonStyle: ButtonStyle {
         configuration.label
             .padding()
             .background(Color.indigo)
-            .cornerRadius(10)
+            .cornerRadius(15)
             .scaleEffect(configuration.isPressed ? 1.5 : 1)
             .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
             
